720dc4f44fd44b882184c66b2a8b6fdac9b617d0
Martin Hooper <martin@mjhooper.co.uk>
2025-10-08 08:28:12 +0100

    changed from init.vim to init.lua

---
+++ init.lua
@@ -0,0 +1,29 @@
+vim.o.syntax="true"
+vim.o.termguicolors=True
+vim.o.tabstop = 4
+vim.o.softtabstop=4
+vim.o.expandtab=true
+vim.o.smartindent=true
+vim.o.incsearch=true
+vim.o.hlsearch=false
+vim.o.splitbelow=true
+vim.o.splitright=true
+vim.o.hidden=true
+vim.o.scrolloff=8
+vim.o.undofile=true
+vim.o.mouse = "a"
+vim.o.ignorecase=true
+vim.o.smartcase=true
+vim.o.showmode=false
+
+-- Setup plugins
+
+local plug=vim.fn['plug#']
+vim.call("plug#begin")
+plug ('itchyny/lightline.vim')
+vim.call("plug#end")
+
+require ('settings')
+
+-- vim.cmd "colorscheme darkblue"
+

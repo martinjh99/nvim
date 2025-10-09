8d66dfb85af0f9a3770876788c57b6c8a570516d
Martin Hooper <martin@mjhooper.co.uk>
2025-10-09 18:23:20 +0100

    updated treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
    "nvim-treesitter/nvim-treesitter",
    build = ":TSUpdate",
    config=function()
-  :-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
